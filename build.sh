ef46aa224ff29d46613aeedd48ba6a34f3f8baef
Douglas Drinka <ddrinka@gmail.com>
2018-11-15 01:26:05 -0700

    Add Nest to build/run docker scripts.

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ docker build -t telegraf telegraf
 docker build -t influxdb influxdb
 docker build -t grafana grafana
 docker build -t motorola_cm -f src/Monitoring.MotorolaCableModem.App/Dockerfile .
+docker build -t nest -f src/Monitoring.Nest.App/Dockerfile .
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 docker run -d --restart always --network monitoring -v /var/local/influx:/var/lib/influxdb --name influxdb influxdb
 docker run -d --restart always --network monitoring -v /etc/cm_passwd:/app/password.txt --name motorola_cm motorola_cm
+docker run -d --restart always --network monitoring -v /etc/nest_passwd:/app/password.txt --name nest nest
 docker run -d --restart always --network monitoring --name telegraf telegraf
