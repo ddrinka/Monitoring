19faba93142fd48502b5e1718d0ba7dbf9bc712a
Douglas Drinka <ddrinka@gmail.com>
2018-12-01 12:14:51 -0600

    Added grafana run commands. Added Nest dashboard. Fixed telegraf weather sources.

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ docker run -d --restart always --network monitoring -v /var/local/influx:/var/li
 docker run -d --restart always --network monitoring -v /etc/cm_passwd:/app/password.txt --name motorola_cm motorola_cm
 docker run -d --restart always --network monitoring -v /etc/nest_passwd:/app/password.txt --name nest nest
 docker run -d --restart always --network monitoring --name telegraf telegraf
+docker run -d --restart always --network monitoring -v /var/local/grafana:/var/lib/grafana -p 80:3000 --name grafana grafana
