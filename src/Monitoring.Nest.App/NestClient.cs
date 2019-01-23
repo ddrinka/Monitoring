aa1a9c593e0da12783f65ccc5415de7d8fb77927
Douglas Drinka <ddrinka@gmail.com>
2019-01-23 16:51:20 -0700

    Include a UserAgent header on the login call.

--- src/Monitoring.Nest.App/NestClient.cs
+++ src/Monitoring.Nest.App/NestClient.cs
@@ -97,6 +97,7 @@ namespace Monitoring.Nest.App
             string endpoint = _loginUrl
                 .AppendPathSegment("session");
             return await _flurl.Request(endpoint)
+                .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
                 .PostJsonAsync(new { Email = _email, Password = _password })
                 .ReceiveJson<Session>();
         }
