0f8c88dd11b133b6004afb361200c2f41839a873
Douglas Drinka <ddrinka@gmail.com>
2020-11-26 01:04:18 -0700

    Added explicit port for influx

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d --restart always --network monitoring -v /var/local/influx:/var/lib/influxdb --name influxdb influxdb
+docker run -d --restart always --network monitoring -v /var/local/influx:/var/lib/influxdb -p 8086:8086 --name influxdb influxdb
 docker run -d --restart always --network monitoring -v /etc/cm_passwd:/app/password.txt --name motorola_cm motorola_cm
 docker run -d --restart always --network monitoring -v /etc/nest_passwd:/app/password.txt --name nest nest
 docker run -d --restart always --network monitoring --name telegraf telegraf
