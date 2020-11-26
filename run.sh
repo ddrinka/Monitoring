2e1e78619e749e560961a0ae083bc6c5c0ef3c2a
Douglas Drinka <ddrinka@gmail.com>
2020-11-26 01:45:24 -0700

    Removed hardcoded bearer token from Meater configuration. Updated to telegraf 1.16

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 docker run -d --restart always --network monitoring -v /var/local/influx:/var/lib/influxdb -p 8086:8086 --name influxdb influxdb
 docker run -d --restart always --network monitoring -v /etc/cm_passwd:/app/password.txt --name motorola_cm motorola_cm
 docker run -d --restart always --network monitoring -v /etc/nest_passwd:/app/password.txt --name nest nest
-docker run -d --restart always --network monitoring --name telegraf telegraf
+docker run -d --restart always --network monitoring -v /etc/meater_key:/etc/meater_key --name telegraf telegraf
 docker run -d --restart always --network monitoring -v /var/local/grafana:/var/lib/grafana -p 80:3000 --name grafana grafana
